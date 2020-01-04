5e6f9af1a95bf863dc06f1d1272906872b45c6d2
cornellekacy <cornellekacy4@gmail.com>
2020-01-04 22:11:04 +0400

    2

--- contact.php
+++ contact.php
@@ -1,65 +1,6 @@
 <?php include 'header.php'; ?>
 		<!-- End Header -->
-    <?php
-/**
- * This example shows how to handle a simple contact form.
- */
 
-//Import PHPMailer classes into the global namespace
-use PHPMailer\PHPMailer\PHPMailer;
-$msg = '';
-//Don't run this unless we're handling a form submission
-if (array_key_exists('email', $_POST)) {
-    date_default_timezone_set('Etc/UTC');
-    require 'autoload.php';
-    //Create a new PHPMailer instance
-    $mail = new PHPMailer;
-    //Tell PHPMailer to use SMTP - requires a local mail server
-    //Faster and safer than using mail()
-    $mail->isSMTP();
-$mail->SMTPSecure = 'tls';
-$mail->Host = 'smtp.gmail.com';
-$mail->Port = 587;
-//Whether to use SMTP authentication
-$mail->SMTPAuth = true;
-//Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = "cornellekacy4@gmail.com";
-//Password to use for SMTP authentication
-$mail->Password = "cornellekacy456";
-    //Use a fixed address in your own domain as the from address
-    //**DO NOT** use the submitter's address here as it will be forgery
-    //and will cause your messages to fail SPF checks
-    $mail->setFrom('from@example.com', 'First Last');
-    //Send the message to yourself, or whoever should receive contact for submissions
-    $mail->addAddress('contact@petflyrelocation.com', 'Contact');
-    //Put the submitter's address in a reply-to header
-    //This will fail if the address provided is invalid,
-    //in which case we should ignore the whole request
-    if ($mail->addReplyTo($_POST['email'], $_POST['name'])) {
-        $mail->Subject = 'Petfly Relocation';
-        //Keep it simple - don't use HTML
-        $mail->isHTML(false);
-        //Build a simple message body
-        $mail->Body = <<<EOT
-Email: {$_POST['email']}
-Name: {$_POST['name']}
-Subject: {$_POST['subject']}
-Message: {$_POST['message']}
-EOT;
-        //Send the message, check for errors
-        if (!$mail->send()) {
-            //The reason for failing to send will be in $mail->ErrorInfo
-            //but you shouldn't display errors to users - process the error, log it on your server.
-            $msg = 'Sorry, something went wrong. Please try again later.'. $mail->ErrorInfo;
-        } else {
-            echo "<script>alert('Message Successfully Sent we will get back to you shortly');
-            window.location.href = 'contact.php'</script>";
-        }
-    } else {
-        $msg = 'Invalid email address, message ignored.';
-    }
-}
-?>
 		<!-- page-banner-section 
 			================================================== -->
 		<section class="page-banner-section contact">
@@ -89,7 +30,7 @@ EOT;
 						
 						</div>
 						<div class="col-md-8">
-							<form id="contact-form" method="post">
+							<form id="contact-form" method="post" action="send.php">
 								<h2>We’re Ready to Help!</h2>
 								<p>we are always happy to help, just fill the form bellow and we will get back to you in less than no time</p>
 								<div class="row">
@@ -101,16 +42,14 @@ EOT;
 									</div>
 								</div>
 								<input name="subject" type="text" placeholder="Subject*" required="">		
-								<textarea name="comment" placeholder="Message*"></textarea>
+								<textarea name="message" placeholder="Message*"></textarea>
 								<p>
 									<span class="call">call us: +1 (720) 791-9099</span>
 									<span class="call">Email: contact@petflyrelocation.com</span> or <br>
 									<input type="submit"  value="Send us a message">
 								</p>
 								
-								<div id="msg" class="message alert">
 								
-								</div>
 							</form>
 						</div>
 						<div class="col-md-2">
